5070d8ab2295a5e77ce5825aabf6476619455437
rittythomas <rittythomaz@gmail.com>
2018-07-21 20:46:02 +0530

    Url changed

--- bookswag/urls.py
+++ bookswag/urls.py
@@ -13,14 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-<<<<<<< HEAD
-from django.urls import urls
 
-urlpatterns = [
-    urls('admin/', admin.site.urls),
-=======
-from django.conf.urls import url
+from django.conf.urls import url,include
+from django.contrib import admin
 
 urlpatterns = [
     url('admin/', admin.site.urls),
